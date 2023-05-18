37969eb23347da512965fa5a2c5a41508c1ec178
Choi-HyunHo <x7788@naver.com>
2023-05-18 13:16:59 +0900

    feat : 로그인 페이지 메타정보 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,11 @@
 import Form from "@/components/login/Form";
 import React from "react";
 
+export const metadata = {
+	title: "Memoji - 로그인",
+	description: "Memoji 로그인 페이지",
+};
+
 export default function login() {
 	return (
 		<section className="flex flex-col items-center m-20 ">
