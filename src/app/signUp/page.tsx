de603d09fa82a22179fc2a006b59b7d7828d173f
Choi-HyunHo <x7788@naver.com>
2023-05-19 10:13:04 +0900

    refactor : 회원 가입 폼 상태 값 추가

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -2,11 +2,16 @@ import SemiHeader from "@/components/SemiHeader";
 import SignUpForm from "@/components/signUp/SignUpForm";
 import React from "react";
 
+export const metadata = {
+	title: "Memoji - 회원가입",
+	description: "Memoji 회원가입 페이지",
+};
+
 export default function signUpPage() {
-    return (
-        <>
-            <SemiHeader img={"💭"} content={"당신이 궁금해요 👀"} />
-            <SignUpForm />
-        </>
-    );
+	return (
+		<>
+			<SemiHeader img={"💭"} content={"당신이 궁금해요 👀"} />
+			<SignUpForm />
+		</>
+	);
 }
--- src/components/signUp/SignUpForm.tsx
+++ src/components/signUp/SignUpForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../Button";
 
 // datepicker
@@ -8,57 +8,103 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function SignUpForm() {
-    const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+	// 달력 상태 값
+	const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
-    return (
-        <div className="flex flex-col items-center">
-            <form className="flex flex-col m-4">
-                <input type="text" name="email" placeholder="Email" required />
+	const [form, setForm] = useState({
+		email: "",
+		password: "",
+		nickname: "",
+		phone: "",
+		date: selectedDate?.toISOString().slice(0, 10).replace(/-/g, ""),
+		address: "",
+	});
 
-                <div style={{ height: "10px" }}></div>
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setForm({
+			...form,
+			[name]: value,
+		});
+	};
 
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    required
-                />
+	// 아래 코드가 없으면 날짜가 변경이 안됨(Form 안에서) 필수
+	useEffect(() => {
+		setForm((prevForm) => ({
+			...prevForm,
+			date: selectedDate?.toISOString().slice(0, 10).replace(/-/g, ""),
+		}));
+	}, [selectedDate]);
 
-                <input
-                    type="text"
-                    name="Nickname"
-                    placeholder="Nickname"
-                    required
-                />
+	const handleDateChange = (date: Date | null) => {
+		console.log(date);
+		setSelectedDate(date);
+	};
 
-                <input
-                    type="tel"
-                    name="phoneNumber"
-                    placeholder="Phone Number"
-                    required
-                />
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(form);
+	};
 
-                <DatePicker
-                    dateFormat="yyyy.MM.dd" // 날짜 형태
-                    shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
-                    minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
-                    maxDate={new Date()} // maxDate 이후 날짜 선택 불가
-                    selected={selectedDate}
-                    onChange={(date) => setSelectedDate(date)}
-                    showYearDropdown
-                    scrollableYearDropdown
-                    yearDropdownItemNumber={15}
-                />
+	return (
+		<div className="flex flex-col items-center">
+			<form className="flex flex-col m-4" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="email"
+					placeholder="Email"
+					required
+					onChange={onChange}
+				/>
 
-                <input
-                    type="text"
-                    name="address"
-                    placeholder="Address"
-                    required
-                />
+				<div style={{ height: "10px" }}></div>
 
-                <Button text={"Sign Up"} />
-            </form>
-        </div>
-    );
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+					required
+					onChange={onChange}
+				/>
+
+				<input
+					type="text"
+					name="nickname"
+					placeholder="Nickname"
+					required
+					onChange={onChange}
+				/>
+
+				<input
+					type="tel"
+					name="phone"
+					placeholder="Phone Number"
+					required
+					onChange={onChange}
+				/>
+
+				<DatePicker
+					dateFormat="yyyy.MM.dd" // 날짜 형태
+					shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
+					minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
+					maxDate={new Date()} // maxDate 이후 날짜 선택 불가
+					selected={selectedDate}
+					onChange={handleDateChange}
+					showYearDropdown
+					scrollableYearDropdown
+					yearDropdownItemNumber={15}
+				/>
+
+				<input
+					type="text"
+					name="address"
+					placeholder="Address"
+					required
+					onChange={onChange}
+				/>
+
+				<Button text={"Sign Up"} />
+			</form>
+		</div>
+	);
 }
