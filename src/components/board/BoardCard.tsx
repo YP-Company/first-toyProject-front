11914248ce1c077bac0722e749d672ea76a0e401
Choi-HyunHo <x7788@naver.com>
2023-05-19 11:52:05 +0900

    style : BoardCard hover 효과 변경

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -9,7 +9,7 @@ export default function BoardCard({ board: { _id, title, content } }: any) {
 	return (
 		<li
 			onClick={() => router.push(`/posts/${_id}`)}
-			className="rounded-2xl overflow-hidden cursor-pointer hover:scale-95 transition-all bg-white h-80 shadow-lg shadow-white-500/50"
+			className="rounded-2xl overflow-hidden cursor-pointer hover:-translate-y-3 transition-all bg-white h-80 shadow-lg shadow-white-500/50"
 		>
 			<div className="mt-2 px-2 text-lg flex justify-between items-center border-solid border-b-2 border-gray-50">
 				<h3 className="truncate">{title}</h3>
