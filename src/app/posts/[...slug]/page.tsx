ebb12d80b92359bccffe5530e8852a28ac758cb3
최현호 <x7788@naver.com>
2023-05-20 19:29:39 +0900

    style : BoardCard 스타일 작업

--- src/app/posts/[...slug]/page.tsx
+++ src/app/posts/[...slug]/page.tsx
@@ -3,18 +3,25 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 interface Props {
-	params: {
-		slug: string;
-	};
+    params: {
+        slug: string;
+    };
 }
 
 export default async function postDetail({ params }: Props) {
-	console.log(params.slug[0]);
-	const post = await getPost(params.slug[0]);
+    console.log(params.slug[0]);
+    const post = await getPost(params.slug[0]);
 
-	if (post?._id === undefined) {
-		redirect("/");
-	}
+    if (post?._id === undefined) {
+        redirect("/");
+    }
 
-	return <div>postDetail : {post.title}</div>;
+    return (
+        <section>
+            <div>
+                <h1>{post.title}</h1>
+                <p>{post.content}</p>
+            </div>
+        </section>
+    );
 }
--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -2,21 +2,41 @@
 
 import { useRouter } from "next/navigation";
 import React from "react";
+import Image from "next/image";
 
-export default function BoardCard({ board: { _id, title, content } }: any) {
-	const router = useRouter();
+export default function BoardCard({
+    board: { _id, title, content, date, author },
+}: any) {
+    const router = useRouter();
 
-	return (
-		<li
-			onClick={() => router.push(`/posts/${_id}`)}
-			className="rounded-2xl overflow-hidden cursor-pointer hover:-translate-y-3 transition-all bg-white h-80 shadow-lg shadow-white-500/50"
-		>
-			<div className="mt-2 px-2 text-lg flex justify-between items-center border-solid border-b-2 border-gray-50">
-				<h3 className="truncate">{title}</h3>
-			</div>
-			<span className="inline-block mt-4 px-2 text-gray-600">
-				{content}
-			</span>
-		</li>
-	);
+    return (
+        <li
+            onClick={() => router.push(`/posts/${_id}`)}
+            className="rounded-xl overflow-hidden cursor-pointer hover:-translate-y-3 transition-all bg-white h-96 shadow-lg shadow-white-500/50 relative"
+        >
+            <div className="w-full flex justify-center">
+                <div className="w-full">
+                    <Image
+                        src="https://res.cloudinary.com/choihyunho/image/upload/v1681125228/cld-sample-5.jpg"
+                        alt="women"
+                        width={300}
+                        height={1}
+                    />
+                </div>
+            </div>
+
+            <div className="mt-2 px-2 text-lg flex justify-between items-center border-solid border-b-2 border-gray-50">
+                <h3 className="truncate">{title}</h3>
+            </div>
+
+            <div className="overflow-hidden m-2">
+                <p className=" text-gray-600 line-clamp-3">{content}</p>
+            </div>
+
+            <div className="flex justify-around absolute bottom-0 left-0 right-0 mb-4">
+                <div>{author}</div>
+                <div>{date}</div>
+            </div>
+        </li>
+    );
 }
