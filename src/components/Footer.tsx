b0247810116f3c86beaba1562790c7c9ea9e0629
최현호 <x7788@naver.com>
2023-05-28 18:55:20 +0900

    style : footer padding 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Footer() {
-	return (
-		<footer className="w-full text-center translate-y-14">
-			<span className="opacity-40">
-				Copyright 2023. YP-Company All rights reserved.
-			</span>
-		</footer>
-	);
+    return (
+        <footer className="w-full pb-12 text-center translate-y-14">
+            <span className="opacity-40">
+                Copyright 2023. YP-Company All rights reserved.
+            </span>
+        </footer>
+    );
 }
